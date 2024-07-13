31e802a34ec74539dc2b8bbdfac1d4cb1ecda497
Becky <beckyngan28@gmail.com>
2024-07-13 16:47:44 +1200

    adding character

--- src/customise.js
+++ src/customise.js
@@ -1,29 +1,34 @@
 import React, { useState } from 'react';
 import './Components/Customise/style.css';
+import bodyImg from './assets/IMG_0773.PNG';
 
 const Customise = () => {
 
-    const [dressupState, setDressupState] = useState({
-        hair: {current: 0, total: 2},
-        face: {current: 0, total: 2},
-        outfit: {current: 0, total: 2},
-      });
+   
 
     return (
 
         <div className='wrapper'>
-        
-            <h1 className="text-4xl ">Ease Elves</h1>
-
-            <div className="input-box">
-                <input type="text" placeholder='name' required/>
+            <h1 className="text-4xl" style={{ position: 'fixed', top: 0, left: 50 }}>
+                ease elves
+            </h1>
+            <div className="image-container">
+            <img src = {bodyImg} alt = "body"/>
             </div>
 
-            
+            <div className="input-box" style={{ position: 'fixed', bottom: '20px', left: '50%', transform: 'translateX(-50%)' }}>
+                <input type="text" placeholder='name' required/>
+            </div>
+    
 
 
 
 {/* 
+ const [dressupState, setDressupState] = useState({
+        hair: {current: 0, total: 2},
+        face: {current: 0, total: 2},
+        outfit: {current: 0, total: 2},
+      });
             <div id="body"></div>
             <div id="hair" className="hair"></div>
             <div id="face" className="face"></div>
