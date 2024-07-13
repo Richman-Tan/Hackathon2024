6515704a1014f2870e63f7645f055d53ee97f4e6
rtan654 <rtan654@aucklanduni.ac.nz>
2024-07-13 14:34:09 +1200

    Adding everything to git hub with routes.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import LoginForm from './Components/LoginForm/LoginForm';
+import Customise from "./Customise.js";
+import Vent from "./Vent.js";
 
-function App() {
-  return (
-    <div>
-      <LoginForm/>
-    </div>
-  );
+export default function App() {
+    return (
+        <BrowserRouter>
+            <div>
+                <Routes>
+                    <Route path="/login"
+                        element={<LoginForm />} />
+
+                    <Route path="/customise"
+                        element={<Customise />} />
+
+                    <Route path="/vent"
+                        element={<Vent />} />
+
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
-
-export default App;
---
+++ src/customise.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Customise = () => {
+    return (
+
+        <div>
+            <div className="min-h-screen flex flex-col">
+                <header className="bg-indigo-500 text-white p-4">
+                    <h1 className="text-3xl font-bold">customise test</h1>
+                </header>
+                <h1>Hello world</h1>
+
+            </div>
+        </div>
+    );
+};
+
+export default Customise;
