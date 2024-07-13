75739bb869bf9f5408be703e06850f7a34d26966
Becky <beckyngan28@gmail.com>
2024-07-13 16:10:39 +1200

    adding to customise

--- src/customise.js
+++ src/customise.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Components/Customise/style.css';
 
 const Customise = () => {
+
+    const [dressupState, setDressupState] = useState({
+        hair: {current: 0, total: 2},
+        face: {current: 0, total: 2},
+        outfit: {current: 0, total: 2},
+      });
+
     return (
 
-        <div>
-            <div className="min-h-screen flex flex-col">
-                <header className="bg-indigo-500 text-white p-4">
-                    <h1 className="text-3xl font-bold">customise test</h1>
-                </header>
-                <h1>Hello world</h1>
+        <div className='wrapper'>
+        
+            <h1 className="text-4xl ">Ease Elves</h1>
 
+            <div className="input-box">
+                <input type="text" placeholder='name' required/>
             </div>
+
+            
+
+
+
+{/* 
+            <div id="body"></div>
+            <div id="hair" className="hair"></div>
+            <div id="face" className="face"></div>
+            <div id="outfit" className="outfit"></div> */}
+
         </div>
     );
 };
