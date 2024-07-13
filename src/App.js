1c061a27c86f99bcccae5f9f4439c089da200caa
rtan654 <rtan654@aucklanduni.ac.nz>
2024-07-14 01:29:08 +1200

    Trying to use socket.io but its so hard to use and the sending message function is not sending over.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import LoginForm from './Components/LoginForm/LoginForm';
-import Customise from "./Customise.js";
+import Customise from "./Customise.js"; // Fix the file name casing here
 import Vent from "./Components/Vent/Vent.jsx";
+import Send from "./Components/SendMessage/send.js";
 
 export default function App() {
 	return (
@@ -18,6 +19,9 @@ export default function App() {
 
 					<Route path="/vent"
 						element={<Vent />} />
+
+					<Route path="/send"
+						element={<Send />} />
 
 				</Routes>
 			</div>
