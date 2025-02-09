70827d53d4991231b6e6f40249f11414a8d3a395
Richman Tan <rtan654@aucklanduni.ac.nz>
2025-02-09 20:30:34 +1300

    feat: set up backend with initial configuration

---
+++ server/connect.cjs
@@ -0,0 +1,21 @@
+const { MongoClient, Collection } = require('mongodb');
+require('dotenv').config({path: './config.env'});
+
+async function main() {
+
+    const Db = process.env.Atlas_URI;
+    const client = new MongoClient(Db);
+
+    try{
+        await client.connect();
+
+        const collections = await client.db("sample_mflix").collections();
+        collections.forEach((Collection) => console.log(Collection.s.namespace.collection));
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+main()
